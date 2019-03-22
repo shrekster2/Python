82eab197b2554ddb12fe200d177c5aef209a8bdf
Thenonceymann <38389668+shrekster2@users.noreply.github.com>
2019-03-22 11:53:33 -0700

    Update Apple screen saver.py

--- Apple screen saver.py
+++ Apple screen saver.py
@@ -1,4 +1,4 @@
-
+# there was a mac screensaver in the 90's that basically said quotes that started with "I will" or "I will not" so I made something like it in python.
 import random
 things = ['I will not force feed peanuts to the kid with peanut allergy', 'I will not put the lighter fluid by the fireplace', 'I will not insult the man with a chainsaw', 'I will not film my friends asleep', 'I will not be culturally insensitive on live TV', 'I will not raise the dead', 'I will not intentionally poison friends', 'I will not run down my neighbors in my car', 'I will not rip my pants off at my brothers wedding', 'I will not commit tax fraud', 'I will not eat the cars tires', 'I will not cheat on the presidents wife', 'I will not start fires during class',  ]
 print(random.choice(things))
