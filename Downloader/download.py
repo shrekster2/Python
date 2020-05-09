1480d9506a2584bc0a30871bf3a4e23a5cfded9a
Thenonceymann <38389668+shrekster2@users.noreply.github.com>
2020-05-08 23:55:36 -0300

    Add files via upload

--- Downloader/download.py
+++ Downloader/download.py
@@ -21,17 +21,6 @@ os.system("sudo apt-get install filezilla")
 print("Installing wine, this may take a while")
 os.system("sudo dpkg --add-architecture i386")
 os.system("wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -")
-print("16 = ubuntu 16.04, 18 = ubuntu 18.04, 19 = ubuntu 19.10, 20 = ubuntu 20.04")
-C = input("Which one do you have: ")
-if C == "16":
-    os.system("sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main' ")
-if C == "18":
-    print('Dont need a Repo!')
-if C == "19":
-    os.system("sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main' ")
-if C == "20":
-    os.system("sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' ")
-
 os.system("sudo apt update")
 
 D = input("Do you want stable = stable, stag = staging or d = developement")
